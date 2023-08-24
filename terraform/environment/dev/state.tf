3a98c31fb9b2904275a22ccf85deb8111aa88236
mrUnknown7 <50827756+mrUnknown7@users.noreply.github.com>
2023-08-24 13:08:04 +0100

    Update state.tf

--- terraform/environment/dev/state.tf
+++ terraform/environment/dev/state.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket = "jenkins-terraform-state-files"
     key = "terraform/dev.tfstate"
-    access_key = "AKIATUETLGXC4GUOFFMY"
-    secret_key = "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"
     region = "ap-south-1"
   }
 }
